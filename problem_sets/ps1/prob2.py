3e1134a89f77e3c8ba8aa79475d9fbead23c02f7
Camryn Mullin <camryn.mullin@mail.mcgill.ca>
2021-09-14 20:20:48 -0400

    done problem 2

--- problem_sets/ps1/prob2.py
+++ problem_sets/ps1/prob2.py
@@ -10,10 +10,15 @@ def ndiff(fun,x,full=False):
     eps = 2**-52 
     dx = eps**(1/3)
     f0, f1, f2 = fun(x), fun(x+dx), fun(x-dx)
-    deriv = (f1-f2)/(2*dx)
-    err = deriv/f0 - 1
- 
+    deriv = (f1-f2)/(2*dx) #the derivative
+    err = (eps*f0)**(2/3)
+    #err = f(x)*eps/dx + 1/6*f^3(x)*dx**2
+    
+    
     if full==True:
+        deriv_2 = (f1+f2-2*f0)/dx**2
+        deriv_3 = (fun(x+2*dx)-2*f1-fun(x-2*dx)+2*f2)/(2*dx**3)
+        err = f0*eps/dx + 1/6*deriv_3*dx**2 #the error
         return deriv, err
     return deriv
 
