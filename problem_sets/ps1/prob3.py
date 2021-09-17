bded16008b48e5dec9fb588f5612a1bab9157786
Camryn Mullin <camryn.mullin@mail.mcgill.ca>
2021-09-17 13:27:55 -0400

    clean up of 3 and 4

--- problem_sets/ps1/prob3.py
+++ problem_sets/ps1/prob3.py
@@ -11,30 +11,11 @@ V = np.linspace(data[1][0], data[1][-1], 10)
 
 def lakeshore(V,data):
     T_pts = data[0] #tempurature
-    V_pts = data[1] #voltage
-    dV_dT = data[2] #dV/dT    
+    V_pts = data[1] #voltage    
     
-    #try:
-        #npt = len(V)
-    #except TypeError:
-        #V = np.array([V])
-        #npt = len(V)
-    
-    #V_pts2 = V_pts.reshape(-1,1)
-    #V2 = V.reshape(-1,1)
-    #nbrs  = NearestNeighbors(n_neighbors=4, algorithm='ball_tree').fit(V_pts2)
-    #distances, indicies  = nbrs.kneighbors(V2, 4)
-    #V_use = V_pts[indicies]
-    #T_use = T_pts[indicies] 
     V_use, T_use= zip(*sorted(zip(V_pts, T_pts)))
-    
     spln=interpolate.splrep(V_use,T_use)
-    #print((spln))
     T=interpolate.splev(V,spln)
-    #T = np.empty(npt)
-    #for i in range(npt):
-        #poly = np.polyfit(V_use[i],T_use[i],3)
-        #T[i] = np.polyval(poly, V[i])
     
     #the error
     bootstrap1 = np.random.choice(np.linspace(0, len(V_pts)-1, len(V_pts), dtype=int), len(V_pts)) #bootstrap sample of indicies
@@ -50,13 +31,11 @@ def lakeshore(V,data):
         spln = interpolate.splrep(V_use,T_use)
         var.append(np.var(spln[0]))
     err = np.var(var)
-    #err = 'e'
     return T, err
 
 print('At the voltage', V, 'the tempurature is', lakeshore(V,data)[0], 'with uncertainty', lakeshore(V,data)[1])
 plt.plot(data[1],data[0],  label='Temp vs Volt relation')
 plt.plot(V, lakeshore(V,data)[0], '*',  label='interpolated')
-#plt.plot(lakeshore(V,data)[2], lakeshore(V, data)[3],  label='temp points')
 plt.xlabel('Voltage (V)')
 plt.ylabel('Temp (k)')
 plt.legend()
--- problem_sets/ps1/prob4.py
+++ problem_sets/ps1/prob4.py
@@ -4,26 +4,26 @@ from scipy import interpolate
 import matplotlib.pyplot as plt
 
 #polynomial
-def poly(x, y, npt):
-    x_fit=np.linspace(x[0],x[-1],2001)
+def poly(x,y,npt1,npt2):
+    x_fit=np.linspace(x[0],x[-1], npt2)
     y_fit=np.ones(len(x_fit))
-    for i in range(1, npt):
+    for i in range(1, npt1):
         y_fit*=(x_fit-x[i])
     y_fit = y_fit/y_fit[0]*y[0]
     return x_fit, y_fit 
 
 #cubic spline
-def spln(x,y):
-    x_fit=np.linspace(x[0],x[-1],2001)
+def spln(x,y,npt2):
+    x_fit=np.linspace(x[0],x[-1],npt2)
     spln=interpolate.splrep(x,y)
     y_fit=interpolate.splev(x_fit, spln)
     return x_fit, y_fit
 
 #rational funciton
-def rat(x,y, npt, n, m, pinv=False):
+def rat(x,y,npt1,npt2,n,m,pinv=False):
     assert(len(x)==n+m+1)
-    top_mat = np.empty([npt,n+1])
-    bot_mat = np.empty([npt,m])
+    top_mat = np.empty([npt1,n+1])
+    bot_mat = np.empty([npt1,m])
     for i in range(n+1):
         top_mat[:,i] = x**i
     for i in range(m):
@@ -36,7 +36,7 @@ def rat(x,y, npt, n, m, pinv=False):
     p = params[:n+1]
     q = params[n+1:]
     
-    x_fit = np.linspace(x[0],x[-1],2001)
+    x_fit = np.linspace(x[0],x[-1],npt2)
 
     numer = 0
     for i,param in enumerate(p):
@@ -48,29 +48,31 @@ def rat(x,y, npt, n, m, pinv=False):
     
     return x_fit, y_fit, p, q
 
-npt = 10
+npt1 = 10
+npt2 = 2001
+
 #cos(x)
 print('For cos(x)')                                                                 
-x = np.linspace(-np.pi/2, np.pi/2, npt)
+x = np.linspace(-np.pi/2, np.pi/2, npt1)
 y = np.cos(x)
-x_true = np.linspace(x[0],x[-1],2001)
+x_true = np.linspace(x[0],x[-1],npt2)
 y_true = np.cos(x_true)
 
 plt.figure()
 #polynomial 
-x_fit, y_fit = poly(x,y,npt)
+x_fit, y_fit = poly(x,y,npt1,npt2)
 plt.plot(x_fit, y_fit, label = 'poly')
 print('Error poly', y_fit-y_true)
 
 #spline 
-x_fit, y_fit = spln(x,y)
+x_fit, y_fit = spln(x,y,npt2)
 plt.plot(x_fit, y_fit, label = 'spline')
 print('Error spline', y_fit-y_true)
 
 #rational 
 n=4
 m=5
-x_fit, y_fit, p, q = rat(x,y,npt, n, m)
+x_fit, y_fit, p, q = rat(x,y,npt1,npt2,n,m)
 print('Error rat', y_fit-y_true)
 plt.plot(x_fit, y_fit, label = 'rational')
 
@@ -83,34 +85,34 @@ print('--------------------')
 
 #lorentzien
 print('For Lorentzien')
-x = np.linspace(-1,1, npt)
+x = np.linspace(-1,1, npt1)
 y = 1/(1+x**2)
-x_true = np.linspace(x[0],x[-1],2001)
+x_true = np.linspace(x[0],x[-1],npt2)
 y_true = 1/(1+x_true**2)
 
 plt.figure()
 #polynomial
-x_fit, y_fit = poly(x,y,npt)
+x_fit, y_fit = poly(x,y,npt1,npt2)
 plt.plot(x_fit, y_fit, label = 'poly')
 print('Error poly', y_fit-y_true)
 
 #spline
-x_fit, y_fit = spln(x,y)
+x_fit, y_fit = spln(x,y,npt2)
 plt.plot(x_fit, y_fit, label = 'spline')
 print('Error spline', y_fit-y_true)
 
 #rational
 n=4
 m=5
-x_fit, y_fit, p, q = rat(x,y,npt, n,m)
+x_fit, y_fit, p, q = rat(x,y,npt1,npt2,n,m)
 plt.plot(x_fit, y_fit, label = 'rational inv')
-x_true = np.linspace(x[0],x[-1],2001)
+x_true = np.linspace(x[0],x[-1],npt2)
 y_true = 1/(1+x_true**2)
 print('Error rat for np.linalg.inv', y_fit-y_true)
-print('p',p, 'q', q)
-x_fit, y_fit, p, q = rat(x,y, npt, n,m,pinv = True)
+print('p', p, 'q', q)
+x_fit, y_fit, p, q = rat(x,y,npt1,npt2,n,m,pinv = True)
 print('Error rat for np.linalg.pinv', y_fit-y_true)
-print('p',p, 'q', q)
+print('p', p, 'q', q)
 plt.plot(x_fit, y_fit, label = 'rational pinv')
 
 plt.plot(x,y, '*', label = 'points')
