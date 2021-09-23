5dc6d23230d4784733967ee3a85a24a5524dbeda
Camryn Mullin <camryn.mullin@mail.mcgill.ca>
2021-09-22 20:23:11 -0400

    problem2 update

--- problem_sets/ps2/prob2.py
+++ problem_sets/ps2/prob2.py
@@ -5,42 +5,43 @@ def lorentz(x):
     return 1/(1+x**2)
 
 def integrate_adaptive(fun,a,b,tol,extra=None):
-    x = np.linspace(a,b,5)
+    global counter
     if extra is None:
-        iter = 1
+        x = np.linspace(a,b,5)
         y = fun(x)
+        counter += len(y)
     else:
+        x = np.linspace(a,b,5)
         if extra[0] == a:
-            iter = extra[-1]
-            iter =+ 1
-            y = np.concatenate(([extra[1]],fun(x[1:])))
+            y1 = fun(x[1:])
+            y = np.concatenate(([extra[1]],y1))
         else:
-            iter = extra[-1]
-            iter += 1
-            y = np.concatenate((fun(x[:5]),[extra[2]]))
+            y1 = fun(x[:5])
+            y = np.concatenate((y1,[extra[1]]))
+        counter += len(y1)
     dx = (b-a)/(len(x)-1)
     area_coarse = 2*dx*(y[0]+4*y[2]+y[4])/3
     area_fine = dx*(y[0]+4*y[1]+2*y[2]+4*y[3]+y[4])/3
     err = np.abs(area_coarse - area_fine)
     if err<tol:
-        #iter = 1
-        return area_fine, iter
+        return area_fine
     else:
         mid = (a+b)/2
-        left, iterL = integrate_adaptive(fun,a,mid,tol/2,extra=(a,y[0],y[4],iter))
-        right, iterR  = integrate_adaptive(fun,mid,b,tol/2,extra=(b,y[0],y[4],iter))
-        #print(iter)
-        return left+right, iterL+iterR
+        left = integrate_adaptive(fun,a,mid,tol/2,extra=(a,y[0]))
+        right = integrate_adaptive(fun,mid,b,tol/2,extra=(b,y[4]))
+        return left+right
 a = 0
 b = 1
 tol = 1e-7
-exp, iter  = integrate_adaptive(np.exp,a,b,tol)
+counter = 0
+exp = integrate_adaptive(np.exp,a,b,tol)
 #print(exp)
-print('integral exp(x) from 0 to 1 is', exp, 'with',iter, 'function calls')
+print('integral exp(x) from 0 to 1 is', exp, 'with',counter, 'function calls. In class this took 135 function calls, meaning we saved', 135-counter, 'calls.' )
 
 a = -100
 b = 100
-arctan, iter = integrate_adaptive(lorentz,a,b,tol)
+counter = 0
+arctan = integrate_adaptive(lorentz,a,b,tol)
 #print(arctan)
-print('integral lorentz(x) form -100 to 100', arctan, 'with', iter, 'function calls')
+print('integral lorentz(x) form -100 to 100', arctan, 'with', counter, 'function calls. In class this took 5175 function calls, meaning we saved', 5175-counter, 'calls.')
 
