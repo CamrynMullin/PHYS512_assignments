23b421ae28bb4d4379a97c08f59dccabadab5d2b
Camryn Mullin <camryn.mullin@mail.mcgill.ca>
2021-10-05 23:41:34 -0400

    updating 1 and 2 (changes were only made to 2

--- problem_sets/ps3/prob1.py
+++ problem_sets/ps3/prob1.py
@@ -2,8 +2,7 @@
 import numpy as np
 
 def fun(x,y):
-    #dy/dx = y/(1+x**2)
-    global counter
+    global counter #count num function evaluations
     counter += 1       
     return y/(1+x**2)
 
@@ -16,8 +15,8 @@ def rk4_step(fun,x,y,h):
 
 n_steps = 200
 x = np.linspace(-20,20,n_steps)  
-c = 1/np.exp(np.arctan(-20)) #1=c*exp(arctan(-20) from intial conditions
-y_true = c*np.exp(np.arctan(x))#1/(1+20**2)*(1+x**2)#np.exp(np.arctan(x))
+c = 1/np.exp(np.arctan(-20)) #1=c*exp(arctan(-20)) from intial conditions
+y_true = c*np.exp(np.arctan(x))
 y = np.zeros(n_steps)
 y[0] = 1 #y(-20) = 1
 for i in range(n_steps-1):
@@ -27,13 +26,37 @@ for i in range(n_steps-1):
 
 print('The error is', np.std(y-y_true), 'with', counter,'function evaluations per step')   
 
-def rk4_stepd(fun,x,y,h):
-    y1 = rk4_step(fun,x,y,h)
-    y2a = rk4_step(fun,x,y,h/2)
-    y2b = rk4_step(fun,x+h/2,y+y2a,h/2)
-    y2 = y2a + y2b
-    return (4*y2-y1)/3
+#def rk4_stepd(fun,x,y,h):
+    #y1 = rk4_step(fun,x,y,h)
+    #y2a = rk4_step(fun,x,y,h/2)
+    #y2b = rk4_step(fun,x+h/2,y+y2a,h/2)
+    #y2 = y2a + y2b
+    #return (4*y2-y1)/3
 
+def rk4_stepd(fun,x,y,h):
+    #y1 = rk4_step(fun,x,y,h)
+    k1 = h*fun(x,y)
+    k2 = h*fun(x+h/2,y+k1/2)
+    k3 = h*fun(x+h/2,y+k2/2)
+    k4 = h*fun(x+h,y+k3)
+    y1 = (k1 + 2*k2 + 2*k3 + k4)/6     
+    #y2a = rk4_step(fun,x,y,h/2)
+    k1a = k1/2
+    k2a = h/2*fun(x+h/4,y+k1a/2)
+    k3a = h/2*fun(x+h/4,y+k2a/2)
+    k4a = h/2*fun(x+h/2,y+k3a)
+    y2a = (k1a + 2*k2a + 2*k3a + k4a)/6  
+    #y2b = rk4_step(fun,x+h/2,y+y2a,h/2)
+    x += h/2
+    y += y2a
+    k1b = h/2*fun(x,y)
+    k2b = h/2*fun(x+h/4,y+k1b/2)
+    k3b = h/2*fun(x+h/4,y+k2b/2)
+    k4b = h/2*fun(x+h/2,y+k3b)
+    y2b = (k1b + 2*k2b + 2*k3b + k4b)/6  
+    y2 = y2a + y2b
+    return (4*y2-y1)/3    
+    
 y = np.zeros(n_steps)
 y[0] = 1
 for i in range(n_steps-1):
@@ -43,33 +66,15 @@ for i in range(n_steps-1):
 
 print('The error is', np.std(y-y_true), 'with', counter,'function evaluations per step')  
 
-#print('To evaluate with the same number of function calls as the first rk4_step:')
-#y1 = np.zeros(n_steps)
-#y1[0] = y_true[0]#1
-#y2 = np.zeros(n_steps)
-#y2[0] = y_true[0]#1
-#counter = 0
-#for i in range(n_steps-1):
-    #h = x[i+1]-x[i]
-    #y1[i+1] = y1[i]+rk4_step(fun,x[i],y1[i],h)
-    #y2[i+1] = y2[i]+rk4_stepd(fun,x[i],y2[i],h)
-    #k1 = h*fun(x,y)
-    #k2 = h*fun(x+h/2,y+k1/2)
-    #k3 = h*fun(x+h/2,y+k2/2)
-    #k4 = h*fun(x+h,y+k3)
-    #y1 = (k1 + 2*k2 + 2*k3 + k4)/6  
-    #h = h/2
-    #k12a = k1
-    #k22a = h*fun(x+h/2,y+k12a/2)
-    #k32a = h*fun(x+h/2,y+k22a/2)
-    #k42a = h*fun(x+h,y+k32a)
-    #y2a = (k12a + 2*k22a + 2*k32a + k42a)/6 
-    #x = x + h
-    #y = y + y2a
-    #k12b = h*fun(x,y)
-    #k22b = h*fun(x+h/2,y+k12b/2)
-    #k32b = h*fun(x+h/2,y+k22b/2)
-    #k42b = h*fun(x+h,y+k32b)
-    #y2b = (k12b + 2*k22b + 2*k32b + k42b)/6 
-    #y2 = y2a + y2b
-    #return (4*y2-y1)/3    
+print('To evaluate with the same number of function calls as the first rk4_step:')
+print('The allowed number of function evaluations is', (n_steps-1)*4)
+n_steps = (n_steps-1)*4 // 11
+x = np.linspace(-20,20,n_steps) 
+y_true = c*np.exp(np.arctan(x))
+y = np.zeros(n_steps)
+y[0] = 1
+counter = 0
+for i in range(n_steps-1):
+    h = x[i+1]-x[i]
+    y[i+1] = y[i]+rk4_stepd(fun,x[i],y[i],h)
+print('The error for rk2_stepd is', np.std(y-y_true), 'with', counter, 'evaluations') 
--- problem_sets/ps3/prob2.py
+++ problem_sets/ps3/prob2.py
@@ -2,7 +2,8 @@
 import numpy as np
 from scipy import integrate
 import time
-
+import matplotlib.pyplot as plt
+#a)
 #decay rates in seconds
 hours = 60*60 #converting hours to seconds
 years = 365*24*hours #converting years to seconds
@@ -24,12 +25,12 @@ Po210 = 138.376*24*hours
 half_life = np.array([U238,Th234,Pr234,U234,Th230,Ra226,Rn222,Po218,Pb214,Bi214,Po214,Pb210,Bi210,Po210])
 
 def decay_solver(x,y,half_life=half_life):
-    dydx=np.zeros(len(half_life)+1)
-    dydx[0]=-y[0]/half_life[0]
+    dydx = np.zeros(len(half_life)+1)
+    dydx[0] = -y[0]/half_life[0]
     for i in range((len(half_life[1:-1]))):
-        dydx[i] = y[i-1]/half_life[i-1]-y[i]/half_life[i]
-    dydx[-1]=y[-1]/half_life[-1]
-    return dydx    
+        dydx[i] = y[i-1]/half_life[i-1] - y[i]/half_life[i]
+    dydx[-1] = y[-1]/half_life[-1]
+    return dydx
 
 #t0,t1 are limits of integration, y0 is initial condition
 t0 = 0
@@ -40,4 +41,28 @@ t_in = time.time()
 rk4_sol = integrate.solve_ivp(decay_solver,[t0,t1],y0,method='Radau')
 t_fin = time.time()
 print('This took', rk4_sol.nfev,'evaluations and', t_fin-t_in,'seconds with integrate.solve_ivp method = Radau.')
-print('The answers were', rk4_sol.y[0,-1], 'with truth', np.exp(-1*(t1-t0)))
+print('The answers were', rk4_sol.y[:,-1], 'with truth', np.exp(-1*(t1-t0)))
+#b)
+x = np.linspace(0, t1, len(rk4_sol.t[4:]))
+y_U238 = rk4_sol.y[0,4:]
+y_Pb206 = rk4_sol.y[11,4:] #Pb210
+#rk4_sol.y #each cell in axis 0 is for an element
+#rk4_sol.y[0] #27 number of time points
+#print(rk4_sol.y)
+plt.figure()
+plt.plot(x, y_Pb206/y_U238)
+plt.xlabel('Time')
+plt.ylabel('Pb206/U238')
+plt.title('Ratio of Pb206 to U238')
+plt.savefig('Ratio of Pb206 to U238.png')
+plt.show()
+
+y_Th230 = rk4_sol.y[4,4:]
+y_U234 = rk4_sol.y[3,4:] #Pb210
+plt.figure()
+plt.plot(x, y_Th230/y_U234)
+plt.xlabel('Time')
+plt.ylabel('Th230/U234')
+plt.title('Ratio of Th230 to U234')
+plt.savefig('Ratio of Th230 to U234.png')
+plt.show()
