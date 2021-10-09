1823eaa96ba465fcba331a60f2fd52d3ec474249
Camryn Mullin <camryn.mullin@mail.mcgill.ca>
2021-10-08 21:34:14 -0400

    problem 2

--- problem_sets/ps3/prob2.py
+++ problem_sets/ps3/prob2.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy import integrate
 import time
 import matplotlib.pyplot as plt
-#a)
+#part a
 #decay rates in seconds
 hours = 60*60 #converting hours to seconds
 years = 365*24*hours #converting years to seconds
@@ -30,24 +30,27 @@ def decay_solver(x,y,half_life=half_life):
     for i in range((len(half_life)-1)):
         dydx[i+1] = y[i]/half_life[i] - y[i+1]/half_life[i+1]
     dydx[-1] = y[-2]/half_life[-1]
-    return dydx#*np.log(2)
+    return dydx
 
 #t0,t1 are limits of integration, y0 is initial condition
 t0 = 0
-t1 = half_life[0]
+t1 = half_life[0] #run for a single half life
 y0 = np.zeros(len(half_life)+1)
 y0[0] = 1
 t_in = time.time()
 rk4_sol = integrate.solve_ivp(decay_solver,[t0,t1],y0,method='Radau')
 t_fin = time.time()
 print('This took', rk4_sol.nfev,'evaluations and', t_fin-t_in,'seconds with integrate.solve_ivp method = Radau.')
-print('The answers were', rk4_sol.y[:,-1], 'with truth', np.exp(-1*(t1-t0)))
-#b)
-a = 0#15
+
+print('The final values were', rk4_sol.y[:,-1])
+
+#part b
+#ratio of Pb206 to U238
+a = 15
 b = -1
 x = np.linspace(rk4_sol.t[a], rk4_sol.t[b-1], len(rk4_sol.t[a:b]))
 y_U238 = rk4_sol.y[0,a:b]
-y_Pb206 = rk4_sol.y[-1,a:b] #Pb210
+y_Pb206 = rk4_sol.y[-1,a:b] 
 plt.figure()
 plt.plot(x, y_Pb206/y_U238)
 plt.xlabel('Time')
@@ -55,9 +58,9 @@ plt.ylabel('Pb206/U238')
 plt.title('Ratio of Pb206 to U238')
 plt.savefig('Ratio of Pb206 to U238.png')
 plt.show()
-
-a = 0#10
-b = -1#25
+#ratio of Th230 to U234
+a = 13
+b = 23
 x = np.linspace(rk4_sol.t[a], rk4_sol.t[b-1], len(rk4_sol.t[a:b]))
 y_Th230 = rk4_sol.y[4,a:b]
 y_U234 = rk4_sol.y[3,a:b] 
