31f98be378a39748455a4689cd4a87534d91c7a0
Camryn Mullin <camryn.mullin@mail.mcgill.ca>
2021-10-15 15:01:24 -0400

    finishing 2 and 3

--- problem_sets/ps4/prob2.py
+++ problem_sets/ps4/prob2.py
@@ -11,7 +11,7 @@ import prob1
     #f1, f2 = fun(pars1), fun(pars2)
     #deriv = (f1-f2)/(2*dx)
     #return deriv
-def ndiff(fun,x,x1,x2,dx): #version of differentiator from ps1 problem 2 
+def ndiff(fun,x1,x2,dx): #version of differentiator from ps1 problem 2 
     deriv = (fun(x1)-fun(x2))/(2*dx)
     return deriv  
 
@@ -31,37 +31,46 @@ def fit_newton(m0,fun,x,y,noise):
             x1 = m.copy()
             x2 = m.copy()
             dx = 0.01*m[i]
-            x1[i],x2[i] = m[i] + dx, m[i] - dx
-            derivs[:,i] += ndiff(fun,m,x1,x2,dx)
+            x1[i] = m[i] + dx 
+            x2[i] = m[i] - dx
+            derivs[:,i] += ndiff(fun,x1,x2,dx)
             #derivs[:,i] += ndiff(fun,m,i)
         model = fun(m)
         res = y - model
         chisq = chisq_fun(y,model,noise)
         lhs = derivs.T@N_inv@derivs
         rhs = derivs.T@N_inv@res
+        #try:
         dm = np.linalg.inv(lhs)@rhs
+        #except:
+            #print("singular matrix, don't update dm")
+            #dm = dm #np.linalg.pinv(lhs)@rhs
         m += dm
+        #print('after Newtons params are', m)
         print('chisq is ',chisq,' with step ',dm) 
+        #if m[3] < 0: #non-physical
+            #m[3] = 0.06 #back to start
+            #dm[3] = 0 #don't take a step here this round
+            #delta_chisq = 1 #ensure loop continues
+        #else:
         delta_chisq = chisq - chisq_old
         chisq_old = chisq
-    #eps = 2**-52 
-    #dx = eps**(1/3)    
-    #curvature_matrix = ndiff(chisq_fun,model,model+dx,model-dx,dx,double=True)
-    return m, dm, chisq, lhs#, curvature_matrix
+    matrix = np.linalg.inv(lhs)   
+    errs = np.sqrt(np.diag(matrix))
+    return m, errs, chisq, matrix#@derivs
     
 fun = prob1.get_spectrum
 x = prob1.ell
 y = prob1.spec
 errs = prob1.errs
 
-m0 = np.asarray([69,0.022,0.12,0.06,2.1e-9,0.95]) #initial guess
+#m0 = np.asarray([69,0.022,0.12,0.06,2.1e-9,0.95]) #initial guess
+m0 = np.asarray([60,0.02,0.1,0.05,2.00e-9,1.0]) #initial guess
+#"better" pars caused problembs (tau -ve)
 m_fit, m_errs, chisq, matrix = fit_newton(m0,fun,x,y,errs)
 
-#def main():
-#m_errs = np.mean(matrix, axis = 0)
 print('the best fit params are', m_fit, 'with errors', m_errs)
 data = np.column_stack([m_fit,m_errs])
-#print('data', data)
 np.savetxt("planck_fit_params.txt", data)
 np.savetxt("curvture_matrix.txt", matrix)
     
@@ -77,54 +86,3 @@ np.savetxt("curvture_matrix.txt", matrix)
     
 #if __name__ == "__main__":
     #main()
-
-#lm
-#def update_lamda(lamda, success):
-    #if success:
-        #lamda /= 1.5
-        #if lamda < 0.5:
-            #lamda = 0
-    #else:
-        #if lamda==0:
-            #lamda = 1
-        #else:
-            #lamda *= 1.5**2
-    #return lamda  
-
-#def get_matrices(m,fun,x,y,Ninv=None):
-    #model = fun(m,x)
-    #derivs = ndiff(fun,m,x) 
-    #res = y - model
-    #if Ninv is None:
-            #lhs = derivs.T@derivs
-            #rhs = derivs.T@res
-            #chisq = np.sum(res**2)
-    #else:
-        #lhs = derivs.T@Ninv@derivs
-        #rhs = derivs.T@(Ninv@res)
-        #chisq = res.T@Ninv@res
-    #return chisq,lhs,rhs
-
-#def inv(A, lamda):
-    #mat = A + lamda*np.diag(np.diag(A))
-    #return np.linalg.lin(mat)
-    
-#def fit_lm(m,fun,x,y,Ninv=None,niter=10,chitol=0.01):
-    #lamda = 0
-    #chisq,lhs,rhs = get_matrices(m,fun,x,y,Ninv)
-    #for i in range(niter):
-        #lhs_inv = linv(lhs,lamda)
-        #dm = lhs_inv@rhs
-        #chisq_new,lhs_new,rhs_new = get_matrices(m+dm,fun,x,y,Ninv)
-        #if chisq_new < chisq:                                                     
-            #if lamda==0 and (np.abs(chisq-chisq_new)<chitol):
-                #return m+dm
-            #chisq = chisq_new
-            #lhs = lhs_new
-            #rhs = rhs_new
-            #m += dm
-            #lamda = update_lamda(lamda,True)
-        #else:
-            #lamda = update_lamda(lamda,False)
-        #print('on iteration ',i,' chisq is ',chisq,' with step ',dm,' and lamda',lamda)
-    #return m, lhs
--- problem_sets/ps4/prob3.py
+++ problem_sets/ps4/prob3.py
@@ -1,66 +1,101 @@
 #problem 3 260926298
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.constants import h
-import corner
+#import corner
 import prob1
-#import prob2
 
-def get_chisq(pars,x,y,noise,fun):
+def get_chisq(pars,y,noise,fun):
     model = fun(pars)
     return np.sum(((y-model)/noise)**2)
 
-def mcmc(pars,step_size,x,y,fun,noise,nstep=1000):
-    current_chisq = get_chisq(pars,x,y,noise,fun)
+def mcmc(pars,step_size,x,y,fun,noise,nstep=5000):
+    current_chisq = get_chisq(pars,y,noise,fun)
     npar = len(pars)
     chain = np.zeros([nstep,npar])
+    chain_errs = np.zeros([nstep,npar])
     chisq_vec = np.zeros(nstep)
     for i in range(nstep):
         print('step', i)
         trial_pars = pars + step_size*np.random.randn(npar)
-        trial_chisq = get_chisq(trial_pars,x,y,noise,fun)
+        trial_chisq = get_chisq(trial_pars,y,noise,fun)
         delta_chisq = trial_chisq - current_chisq
         accept_prob = np.exp(-0.5*delta_chisq)
-        accept = np.random.rand(1) < accept_prob
+        accept = np.random.rand(1) < accept_prob #always accept if chisq decrease
         if accept:
             pars = trial_pars
             current_chisq = trial_chisq
         chain[i,:] = pars
+        chain_errs[i,:] = np.std(chain, axis=0)
         chisq_vec[i] = current_chisq
-    return chain,chisq_vec
+        print('pars are', pars, 'with errors', chain_errs[i,:])
+        print('chisq is', current_chisq)
+    return chain,chain_errs,chisq_vec
 
-def main():
-    model_fun = prob1.get_spectrum  
-    x = prob1.ell
-    y = prob1.spec
-    errs = prob1.errs
-    #step_size = np.loadtxt("curvture_matrix.txt")
-    step_size = np.loadtxt('planck_fit_params.txt')[:,1]
-    #print(step_size)
-    pars = np.array([69,0.022,0.12,0.06,2.1e-9,0.95])
-    chain, chisq = mcmc(pars,step_size,x,y,model_fun,errs)
-    #step_size_new = np.std(chain,axis=0)
-    #starting_pars = np.mean(chain,axis=0)+3*np.random.randn(npar)*step_size_new
-    #chain2, chisq2 = mcmc(starting_pars,step_size_new,x,y,model_fun,errs)
+#def main():
+model_fun = prob1.get_spectrum  
+x = prob1.ell
+y = prob1.spec
+errs = prob1.errs
+#matrix = np.loadtxt("curvture_matrix.txt")
+step_size = np.loadtxt('planck_fit_params.txt')[:,1]
+pars = np.array([69,0.022,0.12,0.06,2.1e-9,0.95])
+chain, errs, chisq = mcmc(pars,step_size,x,y,model_fun,errs)
+print('The final best fit values were', chain[-1], 'with errors', errs[-1], 'and chisq', chisq[-1])
     
-    data = np.column_stack([chain,chisq])
-    np.savetxt("planck_chain.txt", data)
-    h = chain[-1][0]/100
-    dark_energy = 1 - chain[-1][1]/h**2 - chain[-1][2]/h**2
-    print('Mean value of dark energy is', dark_energy, 'with error', chisq[-1])
+data = np.column_stack([chisq,chain])
+np.savetxt("planck_chain.txt", data)
     
-    ##plotting
-    #plt.figure()
-    #plt.ion()
-    #labels = ['H0', 'Ω_b h^2', 'Ω_c h^2', 'τ', 'A_s', 'n_s']
-    #corner.corner(pars,labels=labels, show_titles=True,title_fmt='.2f')
-    #plt.show()
+#computing dark energy
+h = chain[-1][0]/100
+dark_energy = 1 - chain[-1][1]/h**2 - chain[-1][2]/h**2
+dark_err = errs[-1][1]/h**2 + errs[-1][2]/h**2
+print('Mean value of dark energy is', dark_energy, 'with error', dark_err)
     
-    ##forier transform of each parameter
-    #plt.figure()
-    #for i in range(len(pars[0])):
-        #plt.loglog(np.abs(np.fft.rfft(pars[:,i])))
-    #plt.show()
+#plotting
+plt.figure()
+plt.ion()
+plt.plot(chisq)
+plt.title('Chi square from MCMC')
+plt.savefig('Chisq_prob3.png')
+plt.show()
+
+plt.ion()
+planck_binned = np.loadtxt('COM_PowerSpect_CMB-TT-binned_R3.01.txt',skiprows=1)
+errs_binned = 0.5*(planck_binned[:,2]+planck_binned[:,3]);
+plt.figure()
+plt.plot(x,y, label='data')
+plt.plot(x,model_fun(pars), label='Old Fit'); plt.plot(x,model_fun(chain[-1]), label='MCMC fit')
+plt.errorbar(planck_binned[:,0],planck_binned[:,1],errs_binned,fmt='.', label='error bars')
+plt.legend()
+plt.savefig('q3_power_spectrum.png')
+plt.show()
+
+plt.ion()
+plt.figure()
+plt.plot(x,y-model_fun(pars),'.', label='Old Fit'); plt.plot(x,y-model_fun(chain[-1]), '.',label='MCMC fit')
+plt.errorbar(x,np.zeros(len(x)),prob1.errs,fmt='.', label='error bar')
+plt.legend()
+plt.savefig('q3_residuals.png')
+plt.show()
+
+
+chain_pars = np.array(chain)
+#plt.figure()
+#plt.ion()
+labels = ['H0', 'Ω_b h^2', 'Ω_c h^2', 'τ', 'A_s', 'n_s']
+
+#corner.corner(chain_pars[20:],labels=labels, show_titles=True,title_fmt='.2f')
+#plt.savefig('corner_prob3.png')
+#plt.show()
     
-if __name__ == "__main__":
-    main()
+#forier transform of each parameter
+plt.figure()
+for i in range(len(chain_pars[0])):
+    plt.loglog(np.abs(np.fft.rfft(chain_pars[:,i])), label='{}'.format(labels[i]))
+plt.title('Paramater fourier tranform')
+plt.legend()
+plt.savefig('fourier_prob3.png')
+plt.show()
+    
+#if __name__ == "__main__":
+    #main()
